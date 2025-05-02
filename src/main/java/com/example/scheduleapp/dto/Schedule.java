e5724b2c5b11c906fc0c611777ac4c6de9f34eab
qkrehdrl4417 <qkrehdrl4417@naver.com>
2025-05-02 14:47:03 +0900

    schedule 엔티티 작성

---
+++ src/main/java/com/example/scheduleapp/dto/Schedule.java
@@ -0,0 +1,31 @@
+package com.example.scheduleapp.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime dueDate;
+}
